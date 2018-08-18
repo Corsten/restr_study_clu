6756e68a975c7364fbc9cc13396cf89553446547
maratfatkullov <fatk-marat@yandex.ru>
2018-08-18 11:28:09 +0400

    Add rss converter

--- lib/application.rb
+++ lib/application.rb
@@ -13,12 +13,14 @@ class Application
       hash = doc.nil? ? {} : Parser.to_hash(doc)
       items = hash[:rss][:channel][:item]
       #Precess manipulation with hash data. Sort, reverse, etc.
-      processed_data = !items.empty? ? Handler.process(items, options) : {}
+      hash[:rss][:channel][:item] = !items.empty? ? Handler.process(items, options) : {}
       supported_formats = %w[rss atom]
       #Output format by default is rss
       format = supported_formats.include?(options[:format]) ? options[:format] : 'rss'
       #Convert to atom or rss
-      result = Converter.convert(processed_data, format)
+      result = Converter.convert(hash, format)
+      puts "result"
+      puts result
     end
   end
 end
--- lib/converter/rss.rb
+++ lib/converter/rss.rb
@@ -3,14 +3,32 @@ require 'rss'
 class RssConverter
   def self.convert(data)
     rss = RSS::Maker.make("2.0") do |m|
-      m.channel.author  = "Steve Wattam"
+      data[:rss][:channel].each do |key, value|
+        m.channel.author = value if key.to_s == 'author'
+        m.channel.about = value if key.to_s == 'about'
+        m.channel.title = value if key.to_s == 'title'
+        m.channel.link = value[0] if key.to_s == 'link'
+        m.channel.description = value if key.to_s == 'description'
+        m.channel.language = value if key.to_s == 'language'
+      end
       m.channel.updated = Time.now
-      m.channel.about   = "http://stephenwattam.com/blog/"
-      m.channel.title   = "Steve W's Blog"
-      m.channel.link    = "test.com"
-      m.channel.description = "Test"
+      data[:rss][:channel][:item].each do |data_item|
+        puts data_item
+        m.items.new_item do |item|
+          item.guid.content = data_item[:guid] unless data_item[:guid].nil?
+          item.title = data_item[:title] unless data_item[:title].nil?
+          item.link = data_item[:link] unless data_item[:link].nil?
+          item.description = "<![CDATA[#{data_item[:description][:'#cdata-section']}]]>" unless data_item[:description].nil?
+          item.pubDate = data_item[:pubDate] unless data_item[:pubDate].nil?
+          unless data_item[:enclosure].nil?
+            item.enclosure.url = data_item[:enclosure][:url] unless data_item[:enclosure][:url].nil?
+            item.enclosure.length = data_item[:enclosure][:length] unless data_item[:enclosure][:length].nil?
+            item.enclosure.type = data_item[:enclosure][:type] unless data_item[:enclosure][:type].nil?
+          end
+        end
+      end
     end
-
-    puts data
+    #puts rss
+    rss
   end
 end
