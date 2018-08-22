d48bd33e7024026361681ef934456c4633ada6a9
maratfatkullov <fatk-marat@yandex.ru>
2018-08-22 22:05:27 +0400

    Test. Make rss parser test

--- test/parser/test.rb
+++ test/parser/test.rb
@@ -1,11 +1,15 @@
 require 'csv'
 require 'require_all'
 require_rel '../../test/test_helper.rb'
-require_rel '../../lib/application.rb'
 
 class ParserTest < Minitest::Test
-  def test_parser
-    #assert AtomParser.parse('') == 0
+  def test_rss_parser
+    reader = FileReader.new
+    test_data = reader.read('test/fixtures/rssexample')
 
+    parser = AtomParser.new
+    parsed_data = parser.parse(test_data)
+
+    assert parsed_data[:channel][:title] == 'Test Rest title'
   end
 end
